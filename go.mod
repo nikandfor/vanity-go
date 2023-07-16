f93616a59e28b1b2fa95bb56175f58f2d98ae4ff
nikandfor <nikandfor@gmail.com>
2023-07-16 08:38:35 +0400

    draft

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/nikandfor/vanity-go
+module nikand.dev/go/vanity
 
 go 1.20
 
@@ -6,6 +6,7 @@ require (
 	github.com/nikandfor/cli v0.0.0-20230706003607-fc798aebe736
 	github.com/nikandfor/errors v0.8.0
 	github.com/nikandfor/tlog v0.21.4
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -13,5 +14,4 @@ require (
 	github.com/nikandfor/loc v0.5.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
 	golang.org/x/term v0.9.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,11 +5,15 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/nikandfor/cli"
 	"github.com/nikandfor/errors"
 	"github.com/nikandfor/tlog"
 	"github.com/nikandfor/tlog/ext/tlflag"
+	"github.com/nikandfor/tlog/low"
 	"gopkg.in/yaml.v3"
 )
 
@@ -18,6 +22,15 @@ type (
 		Name string `json:"name"` // import prefix
 		VCS  string `json:"vcs"`
 		Repo string `json:"repo"` // url
+	}
+
+	Config struct {
+		Modules []string `json:"modules"`
+		Replace []struct {
+			Prefix string `json:"prefix"`
+			Repo   string `json:"repo"`
+			VCS    string `json:"vcs"`
+		} `json:"replace"`
 	}
 )
 
@@ -33,6 +46,10 @@ func main() {
 	staticCmd := &cli.Command{
 		Name:   "generate,gen,static",
 		Action: staticRun,
+		Flags: []*cli.Flag{
+			cli.NewFlag("output,o", "static", "output directory"),
+			cli.NewFlag("remove,rm", false, "remove static dir before start"),
+		},
 	}
 
 	app := &cli.Command{
@@ -40,6 +57,7 @@ func main() {
 		Description: "tool for making go vanity module names easy to use",
 		Before:      before,
 		Flags: []*cli.Flag{
+			cli.NewFlag("config", "vanity.yaml", "repos"),
 			cli.NewFlag("modules", "", "file with modules"),
 
 			cli.NewFlag("log", "stderr?dm", "log output file (or stderr)"),
@@ -88,9 +106,9 @@ func before(c *cli.Command) error {
 }
 
 func serveRun(c *cli.Command) (err error) {
-	mods, err := parseModules(c.String("modules"))
+	cfg, err := loadConfig(c.String("config"))
 	if err != nil {
-		return errors.Wrap(err, "load modules")
+		return errors.Wrap(err, "load config")
 	}
 
 	l, err := net.Listen("tcp", c.String("listen"))
@@ -101,16 +119,147 @@ func serveRun(c *cli.Command) (err error) {
 	tlog.Printw("serving", "addr", l.Addr())
 
 	err = http.Serve(l, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		tlog.Printw("request", "method", req.Method, "host", req.Host, "path", req.URL.Path, "query", req.URL.RawQuery)
+		var err error
 
-		_ = mods
+		tr := tlog.Start("request", "method", req.Method, "host", req.Host, "path", req.URL.Path, "query", req.URL.RawQuery)
+		defer tr.Finish("err", &err)
+
+		prefix := path.Join(req.Host, req.URL.Path)
+
+		var module string
+
+		for _, mod := range cfg.Modules {
+			if !strings.HasPrefix(prefix, mod) {
+				continue
+			}
+
+			module = mod
+
+			break
+		}
+
+		if module == "" {
+			http.NotFound(w, req)
+			return
+		}
+
+		for _, rep := range cfg.Replace {
+			if !strings.HasPrefix(prefix, rep.Prefix) {
+				continue
+			}
+
+			repo := strings.Replace(module, rep.Prefix, rep.Repo, 1)
+			vcs := rep.VCS
+
+			if vcs == "" {
+				vcs = "git"
+			}
+
+			err = repoPage.Execute(w, Module{
+				Name: prefix,
+				VCS:  vcs,
+				Repo: repo,
+			})
+			if err != nil {
+				err = errors.Wrap(err, "exec page template")
+			}
+
+			return
+		}
+
+		http.NotFound(w, req)
 	}))
 
 	return nil
 }
 
 func staticRun(c *cli.Command) (err error) {
+	cfg, err := loadConfig(c.String("config"))
+	if err != nil {
+		return errors.Wrap(err, "load config")
+	}
+
+	root := c.String("output")
+
+	if c.Bool("remove") {
+		err = os.RemoveAll(root)
+		if err != nil {
+			return errors.Wrap(err, "remove output dir")
+		}
+	}
+
+	var buf low.Buf
+
+	for _, module := range cfg.Modules {
+		for _, rep := range cfg.Replace {
+			if !strings.HasPrefix(module, rep.Prefix) {
+				continue
+			}
+
+			repo := strings.Replace(module, rep.Prefix, rep.Repo, 1)
+			vcs := rep.VCS
+
+			if vcs == "" {
+				vcs = "git"
+			}
+
+			buf = buf[:0]
+
+			err = repoPage.Execute(&buf, Module{
+				Name: module,
+				VCS:  vcs,
+				Repo: repo,
+			})
+			if err != nil {
+				return errors.Wrap(err, "exec page template")
+			}
+
+			var fname string
+
+			domain := strings.IndexRune(module, '/')
+			if domain == -1 {
+				fname = filepath.Join(root, "index.html")
+			} else {
+				modpath := filepath.FromSlash(module[domain:])
+
+				fname = filepath.Join(root, modpath)
+			}
+
+			dir := filepath.Dir(fname)
+
+			tlog.Printw("writing module", "module", module)
+
+			err = os.MkdirAll(dir, 0o755)
+			if err != nil {
+				return errors.Wrap(err, "mkdir")
+			}
+
+			err = os.WriteFile(fname, buf, 0o644)
+			if err != nil {
+				return errors.Wrap(err, "write file")
+			}
+
+			break
+		}
+	}
+
 	return nil
+}
+
+func loadConfig(name string) (*Config, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return nil, errors.Wrap(err, "read file")
+	}
+
+	var c Config
+
+	err = yaml.Unmarshal(data, &c)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshal")
+	}
+
+	return &c, nil
 }
 
 func parseModules(name string) ([]Module, error) {
@@ -136,7 +285,7 @@ func parseModules(name string) ([]Module, error) {
 }
 
 var repoPage = template.Must(template.New("page").Parse(`<!DOCTYPE html>
-{{ define "godoc" }}https://pkg.go.dev/{{ . }}{{ end }}
+{{- define "godoc" }}https://pkg.go.dev/{{ . }}{{ end }}
 <html lang=en-US>
 <head>
 	<meta name="go-import" content="{{ .Name }} {{ .VCS }} {{ .Repo }}">
